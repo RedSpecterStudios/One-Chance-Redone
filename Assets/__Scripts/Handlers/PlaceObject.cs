d52bb4f129f92565b2a6c10ae784483903842cb9
Benjamin Sims <asschaps@outlook.com>
2016-09-08 12:06:43 -0500

    Worked on RayCasting

--- Assets/__Scripts/Handlers/PlaceObject.cs
+++ Assets/__Scripts/Handlers/PlaceObject.cs
@@ -1,13 +1,17 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 public class PlaceObject : MonoBehaviour {
-    
+
+    private Ray _ray;
+    private RaycastHit _hit;
+
 	void Start () {
 	
 	}
-	
-	void Update () {
-	
-	}
+
+    void Update () {
+        if (Physics.Raycast(_ray, out _hit, Mathf.Infinity)) {
+            Debug.DrawLine(_ray.origin, _hit.point, Color.red);
+        }
+    }
 }
