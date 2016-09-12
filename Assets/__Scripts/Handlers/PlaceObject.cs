645b80b38ad2aa18b8942eeb6a39efe9d254b996
Benjamin Sims <asschaps@outlook.com>
2016-09-12 10:44:18 -0500

    More work on RayCasting

--- Assets/__Scripts/Handlers/PlaceObject.cs
+++ Assets/__Scripts/Handlers/PlaceObject.cs
@@ -2,6 +2,9 @@
 
 public class PlaceObject : MonoBehaviour {
 
+    public Camera tpCamera;
+
+    private bool _placing = false;
     private Ray _ray;
     private RaycastHit _hit;
 
@@ -10,8 +13,17 @@ public class PlaceObject : MonoBehaviour {
 	}
 
     void Update () {
-        if (Physics.Raycast(_ray, out _hit, Mathf.Infinity)) {
-            Debug.DrawLine(_ray.origin, _hit.point, Color.red);
+        if (Input.GetKeyDown(KeyCode.Alpha1)) {
+            _placing = !_placing;
+        }
+    }
+
+    void Place () {
+        while (_placing) {
+            _ray=tpCamera.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(_ray, out _hit, Mathf.Infinity)) {
+
+            }
         }
     }
 }
