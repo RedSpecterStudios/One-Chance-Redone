6449d1dc460b3b42fbafdcd54b7483c98a6910de
Benjamin Sims <asschaps@outlook.com>
2016-10-04 10:44:36 -0500

    Range work

--- Assets/Matt/Scripts/BulletShooter.cs
+++ Assets/Matt/Scripts/BulletShooter.cs
@@ -3,23 +3,18 @@ using System.Collections;
 
 public class BulletShooter : MonoBehaviour {
     
-    public float TravelTime;
-    public CreateSplineProfile Spline;
-    public GameObject Source;
-    public GameObject Target;
-
-    public static BulletShooter instance;
-
-    void Start () {
-        instance = this;
-    }
+    public float travelTime;
+    public CreateSplineProfile spline;
+    public GameObject bulletToMove;
+    public GameObject source;
+    public GameObject target;
 
     public void Shoot() {
         GameObject ObjectToMove = GameObject.CreatePrimitive(PrimitiveType.Sphere);
         ObjectToMove.AddComponent<ParticleMover>();
-        ObjectToMove.GetComponent<ParticleMover>().Source = Source;
-        ObjectToMove.GetComponent<ParticleMover>().Target = Target;
-        ObjectToMove.GetComponent<ParticleMover>().Spline = Spline;
-        ObjectToMove.GetComponent<ParticleMover>().Launch(TravelTime);
+        ObjectToMove.GetComponent<ParticleMover>().Source = source;
+        ObjectToMove.GetComponent<ParticleMover>().Target = target;
+        ObjectToMove.GetComponent<ParticleMover>().Spline = spline;
+        ObjectToMove.GetComponent<ParticleMover>().Launch(travelTime);
     }
 }
--- Assets/__Scripts/Towers/Tower Specific/Basic/BasicDefault.cs
+++ Assets/__Scripts/Towers/Tower Specific/Basic/BasicDefault.cs
@@ -9,17 +9,21 @@ public class BasicDefault : MonoBehaviour {
     private float _dotProd;
     private float _fireRate = 5;
     private float _range = 20f;
-    public GameObject _target;
+    private BulletShooter _bulletShooter;
+    private GameObject _target;
     private GameObject _top;
     private Vector3 _dirAB;
     
 	void Start () {
         StartCoroutine(FireTimer(_fireRate));
-        
+
+        _bulletShooter = GetComponent<BulletShooter>();
         _top = transform.FindChild("Top").gameObject;
 	}
 	
 	void Update () {
+        FindTarget();
+
         _dirAB = (_target.transform.position - _top.transform.position).normalized;
         _dotProd = Vector3.Dot(_dirAB, _top.transform.forward);
 
@@ -33,9 +37,17 @@ public class BasicDefault : MonoBehaviour {
         }
     }
 
+    void FindTarget () {
+        Collider[] hitColliders = Physics.OverlapSphere(new Vector3(0,0,0), 10);
+        foreach (Collider target in hitColliders) {
+            Debug.Log(target.gameObject);
+            // http://answers.unity3d.com/questions/208192/see-the-size-of-an-overlap-sphere.html
+        }
+    }
+
     void Fire () {
         if (_dotProd >= 0.9 && _dist < _range) {
-            BulletShooter.instance.Shoot();
+            _bulletShooter.Shoot();
         }
     }
 
