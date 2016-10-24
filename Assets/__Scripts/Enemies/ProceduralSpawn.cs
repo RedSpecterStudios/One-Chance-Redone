2a5ec97dbd6585d5bd142e138ccbaacc4cbd7386
Benjamin Sims <asschaps@outlook.com>
2016-10-24 10:44:02 -0500

    Started Spawning Algorith

--- Assets/__Scripts/Enemies/ProceduralSpawn.cs
+++ Assets/__Scripts/Enemies/ProceduralSpawn.cs
@@ -1,18 +1,60 @@
 using UnityEngine;
-using System.Collections;
 using System.Collections.Generic;
-using CurveExtended;
 
 public class ProceduralSpawn : MonoBehaviour {
+    
+    public static Enemy GetEnemy (List<Enemy> enemies, float totalWeight) {
+        float _randNum = Random.Range(0, totalWeight);
+        Enemy _selectedEnemy = null;
 
-    public Hashtable _enemies;
-
-    void Start () {
-        
+        foreach (Enemy enemy in enemies) {
+            if (_randNum < enemy.Weight) {
+                _selectedEnemy = enemy;
+                break;
+            }
+            _randNum = _randNum - enemy.Weight;
+        }
+        return _selectedEnemy;
     }
 
-    void Update () {
-        
+    void Start () {
+        List<Enemy> enemies = new List<Enemy>();
+        enemies.Add(new Enemy("Test1", 10));
+        enemies.Add(new Enemy("Test2", 15));
+        enemies.Add(new Enemy("Test3", 35));
+        enemies.Add(new Enemy("Test4", 30));
+
+        float _totalWeight = 0f;
+        foreach (Enemy enemy in enemies) {
+            _totalWeight += enemy.Weight;
+        }
+
+        for (int i = 0; i < 100; i++) {
+            Dictionary<string, float> _result = new Dictionary<string, float>();
+
+            Enemy _selectedEnemy = null;
+            for (int j = 0; j < 100; j++) {
+                _selectedEnemy = GetEnemy(enemies, _totalWeight);
+                if (_selectedEnemy != null) {
+                    if (_result.ContainsKey(_selectedEnemy.EnemyTransform)) {
+                        _result[_selectedEnemy.EnemyTransform]++; 
+                    } else {
+                        _result.Add(_selectedEnemy.EnemyTransform, 1);
+                    }
+                }
+            }
+
+            Debug.Log($"1: {_result["Test1"]}, 2: {_result["Test2"]}, 3: {_result["Test3"]}, 4: {_result["Test4"]}");
+        }
     }
 }
 
+public class Enemy {
+    public string EnemyTransform;
+    public float Weight = 0f;
+
+    public Enemy (string t, float w) {
+        EnemyTransform = t;
+        Weight = w;
+    }
+}
