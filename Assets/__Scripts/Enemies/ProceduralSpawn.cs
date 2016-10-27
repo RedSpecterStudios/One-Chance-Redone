14ff14e2bec36ea4ef62fe2072b8385164c19d66
Benjamin Sims <asschaps@outlook.com>
2016-10-27 10:43:04 -0500

    Algorithmic rework.

--- Assets/__Scripts/Enemies/ProceduralSpawn.cs
+++ Assets/__Scripts/Enemies/ProceduralSpawn.cs
@@ -1,7 +1,13 @@
 using UnityEngine;
+using System.Collections;
 using System.Collections.Generic;
 
 public class ProceduralSpawn : MonoBehaviour {
+
+    public Transform mobOne;
+    public Transform mobTwo;
+    public Transform mobThree;
+    public Transform mobFour;
 
     private float _1 = 0;
     private float _2 = 0;
@@ -10,23 +16,26 @@ public class ProceduralSpawn : MonoBehaviour {
     private float _ran = 0;
 
     void Start () {
-        List<Enemy> enemies = new List<Enemy>();
-        enemies.Add(new Enemy("Test1", 10));
-        enemies.Add(new Enemy("Test2", 15));
-        enemies.Add(new Enemy("Test3", 35));
-        enemies.Add(new Enemy("Test4", 30));
+        Dictionary<Transform, float> enemies = new Dictionary<Transform, float>();
+        enemies.Add(mobOne, 15);
+        enemies.Add(mobTwo, 20);
+        enemies.Add(mobThree, 35);
+        enemies.Add(mobFour, 30);
 
         float _totalWeight = 0f;
-        foreach (Enemy enemy in enemies) {
-            _totalWeight += enemy.Weight;
+        foreach (KeyValuePair<Transform, float> enemy in enemies) {
+            _totalWeight += enemy.Value;
+        }
+        for (int i = 0; i < enemies.Count; i++) {
+            _totalWeight += enemies[i];
         }
 
         for (int i = 0; i < 100; i++) {
             Dictionary<string, float> _result = new Dictionary<string, float>();
 
-            Enemy _selectedEnemy = null;
-            for (int j = 0; j < 100; j++) {
-                _selectedEnemy = GetEnemy(enemies, _totalWeight);
+            //Enemy _selectedEnemy = null;
+            for (int j = 0; j < _totalWeight; j++) {
+                //_selectedEnemy = GetEnemy(enemies, _totalWeight);
                 if (_selectedEnemy != null) {
                     if (_result.ContainsKey(_selectedEnemy.EnemyTransform)) {
                         _result[_selectedEnemy.EnemyTransform]++; 
@@ -41,13 +50,11 @@ public class ProceduralSpawn : MonoBehaviour {
             _3 += _result["Test3"];
             _4 += _result["Test4"];
             _ran++;
-
-            Debug.Log($"Test1: {_result["Test1"]}, Test2: {_result["Test2"]}, Test3: {_result["Test3"]}, Test4: {_result["Test4"]}");
         }
-        Debug.Log($"Test1: {_1/_ran}, Test2: {_2/_ran}, Test3: {_3/_ran}, Test4: {_4/_ran}, _ran: {_ran}, Total: {(_1+_2+_3+_4)/_ran}");
+        Debug.Log($"Weight 15: {_1/_ran}, Weight 20: {_2/_ran}, Weight 35: {_3/_ran}, Weight 30: {_4/_ran}, How many times ran: {_ran}, Total: {(_1+_2+_3+_4)/_ran}");
     }
 
-    public static Enemy GetEnemy (List<Enemy> enemies, float totalWeight) {
+    /*public static Enemy GetEnemy (List<Enemy> enemies, float totalWeight) {
         float _randNum = Random.Range(0, totalWeight);
         Enemy _selectedEnemy = null;
 
@@ -59,15 +66,5 @@ public class ProceduralSpawn : MonoBehaviour {
             _randNum = _randNum - enemy.Weight;
         }
         return _selectedEnemy;
-    }
-}
-
-public class Enemy {
-    public string EnemyTransform;
-    public float Weight = 0f;
-
-    public Enemy (string t, float w) {
-        EnemyTransform = t;
-        Weight = w;
-    }
+    }*/
 }
