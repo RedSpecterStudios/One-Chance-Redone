7a69e0f114289d89e24afc3b17f86fc51029db24
Benjamin Sims <asschaps@outlook.com>
2016-10-31 10:44:18 -0500

    More changes, yay

---
+++ Assets/__Scripts/Enemies/WeightedSpawning.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeightedSpawning : MonoBehaviour {
+
+    public Transform spawnPoint;
+    public SubDict[] levelMobs;
+
+    private float _spawnTime = 3;
+    private float _totalWeight = 0f;
+
+    void Start () {
+
+        foreach (SubDict enemy in levelMobs) {
+            _totalWeight += enemy.weight;
+        }
+
+        StartCoroutine(SpawnTimer(_spawnTime));
+
+        /*for (int i = 0; i < 100; i++) {
+            Debug.Log(GetEnemy(levelMobs, _totalWeight));
+        }*/
+    }
+
+    public void SpawnEnemy () {
+        Instantiate(GetEnemy(levelMobs, _totalWeight), spawnPoint, Quaternion.identity);
+    }
+
+    IEnumerator SpawnTimer (float _timeInSeconds) {
+        SpawnEnemy();
+        yield return new WaitForSeconds(_timeInSeconds);
+        StartCoroutine(SpawnTimer(_spawnTime));
+    }
+
+    public static GameObject GetEnemy (SubDict[] enemies, float totalWeight) {
+        float _randNum = Random.Range(0, totalWeight);
+        GameObject _selectedEnemy = null;
+
+        foreach (SubDict enemy in enemies) {
+            if (_randNum <= enemy.weight) {
+                _selectedEnemy = enemy.mob;
+                break;
+            }
+            _randNum = _randNum - enemy.weight;
+        }
+        return _selectedEnemy;
+    }
+}
+
+[System.Serializable]
+public class SubDict {
+    public GameObject mob;
+    public float weight;
+}
