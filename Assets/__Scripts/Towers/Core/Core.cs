107354fe5365b80372be0e83121d4c02645259c4
Benjamin Sims <asschaps@outlook.com>
2017-02-06 09:51:42 -0600

    More Gem Work

---
+++ Assets/__Scripts/Towers/Core/Core.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Core : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.tag == "Enemy") {
+            Destroy(other, 1.5f);
+        } else {
+            print(other.tag);
+        }
+    }
+}
